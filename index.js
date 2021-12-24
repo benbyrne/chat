25be2a84b371b9cacfce1b895aac48d5fd70c288
benbyrne <ben@benbyrne.com.au>
2021-12-24 16:07:48 +1100

    Added client count, changed layout + fixed buttons

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
+var count = io.engine.clientsCount;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  count = io.engine.clientsCount;
+  io.emit('count', count);
+  socket.on("disconnect", (reason) => {
+    count = io.engine.clientsCount;
+    io.emit('count', count);
+  });
 });
 
 io.on('connection', (socket) => {
